4f8d7b3c79ffc7ac3caa5b93b4266fa1b25b0e05
Maria Eduarda Bahls Datsko <147514718+mariabahls@users.noreply.github.com>
2024-10-07 11:01:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criarCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criarCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criarCartao(
+    'Lingua inglessa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
