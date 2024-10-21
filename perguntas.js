ef3a1e1d23643b4f1fd738d7eb327e4998bd0e23
Maria Eduarda Bahls Datsko <147514718+mariabahls@users.noreply.github.com>
2024-10-21 09:27:33 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,46 +1,46 @@
 criarCartao(
-    'Série',
-    'Na série ARROW, qual o nome do amigo de Oliver que após um acidente vira eu inimigo mortal?',
+    'Serie',
+    'Na serie ARROW, qual o nome do amigo de Oliver que apos um acidente vira eu inimigo mortal?',
     'Tommy'
 )
 
 criarCartao(
-    'Série',
-    'Em La Casa de Papel, qual das capitais abaixo não é nome de personagem?',
+    'Serie',
+    'Em La Casa de Papel, qual das capitais abaixo nao e nome de personagem?',
     'Toronto'
 )
 
 criarCartao(
-    'Série',
-    'Na Série Stranger Things, eles precisam abrir um cofre, cuja senha é a constante de?',
+    'Serie',
+    'Na Serie Stranger Things, eles precisam abrir um cofre, cuja senha e a constante de?',
     'Planck'
 )
 
 criarCartao(
-    'Série',
-    'O filme:A menina que matou os pais, são inspirados em fatos reais. Qual caso esse filme retratará?',
+    'Serie',
+    'O filme:A menina que matou os pais, sao inspirados em fatos reais. Qual caso esse filme retratara?',
     'Suzane Von Richthofen'
 ) 
 criarCartao(
-    'Série',
-    'filme:Crepúculo, o acidente que Bela sofre no estacionamento do colégio, ao ser "quase" atingida po uma van, é provocado por?',
+    'Serie',
+    'filme:Crepuculo, o acidente que Bela sofre no estacionamento do colegio, ao ser "quase" atingida po uma van, e provocado por?',
     'Tyler'
 )
 
 criarCartao(
-    'Série',
-    'Na série Elite, com qual objeto Polo mara Marina?',
-    'Um troféu'
+    'Serie',
+    'Na serie Elite, com qual objeto Polo mara Marina?',
+    'Um trofeu'
 )
 
 criarCartao(
-    'Série',
-    'Qual é a série:Um voo aparentamente normal chega ao seu destino cerca de três hora após a partida, passando por algumas turbulências.No entanto, ao desembarcarem, os passageiros descobrem que foram dados como mortos e se passaram cinco anos desde  a decolagem.',
+    'Serie',
+    'Qual e a serie:Um voo aparentamente normal chega ao seu destino cerca de tres hora apos a partida, passando por algumas turbulencias.No entanto, ao desembarcarem, os passageiros descobrem que foram dados como mortos e se passaram cinco anos desde  a decolagem.',
     'Manifest'
 )
 
 criarCartao(
-    'Série',
-    'Na série Elite qual o nome da personagem que ajuda Polo na segunda temporada?',
+    'Serie',
+    'Na serie Elite qual o nome da personagem que ajuda Polo na segunda temporada?',
     'Caetana'
 )
